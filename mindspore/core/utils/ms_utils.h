6429a6842b49a1e834f3cb02564e74a0d106a2a7
limingqi107 <limingqi@huawei.com>
2024-01-22 19:52:33 +0800

    open the default value in kbyk

--- mindspore/core/utils/ms_utils.h
+++ mindspore/core/utils/ms_utils.h
@@ -99,6 +99,9 @@ static inline void SetOMPThreadNum() {
 
   std::string OMP_env = std::to_string(OMP_thread_num);
   (void)SetEnv("OMP_NUM_THREADS", OMP_env.c_str(), 0);
+
+  (void)SetEnv("GRAPH_OP_RUN", "1", 0);
+  (void)SetEnv("MS_ENABLE_ACLNN", "1", 0);
 }
 
 static inline bool IsLittleByteOrder() {
