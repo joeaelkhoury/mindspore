d65766d70647737ffc95d328c560284e512d8103
r1chardf1d0 <xiaruijie@huawei.com>
2024-03-06 21:43:38 +0800

    fix

--- mindspore/ccsrc/pipeline/jit/pi/graph_capture/graph_build.cc
+++ mindspore/ccsrc/pipeline/jit/pi/graph_capture/graph_build.cc
@@ -2278,7 +2278,6 @@ ValueNode *GetBoundSelf(CallNode *call_node) {
     case AObject::kTypeAnyValue:
       self = func_val;
       break;
-    case AObject::kTypeCFunction:
     case AObject::kTypeTraceNode:
     case AObject::kTypeFunction:
       break;
